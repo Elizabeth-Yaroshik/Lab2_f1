9f41f9cbc13ba7a9de30795a1b20d8c3fcece811
Zealivate <liza.yaroshik.06@mail.ru>
2026-05-11 01:35:43 +0300

    Update data_sample.sql

--- data_sample.sql
+++ data_sample.sql
@@ -6,7 +6,7 @@ INSERT INTO Pilots (full_name, birth_date, car_number, comment) VALUES
 
 -- Вставка результатов гонок (pilot_id ссылается на автоматически сгенерированные ID пилотов)
 INSERT INTO RaceResults (pilot_id, race_name, race_date, position, time_seconds) VALUES
-((SELECT pilot_id FROM Pilots WHERE full_name = 'Макс Ферстаппен'), 'Гран-при Майами', '2026-05-03', 5, 5643,222),
+((SELECT pilot_id FROM Pilots WHERE full_name = 'Макс Ферстаппен'), 'Гран-при Майами', '2026-05-03', 5, 5643.222),
 ((SELECT pilot_id FROM Pilots WHERE full_name = 'Льюис Хэмилтон'), 'Гран-при Майами', '2026-05-03', 6, 5653.026),
 ((SELECT pilot_id FROM Pilots WHERE full_name = 'Макс Ферстаппен'), 'Гран-при Японии', '2026-03-29', 8, 5316.08),
 ((SELECT pilot_id FROM Pilots WHERE full_name = 'Ландо Норрис'), 'Гран-при Японии', '2026-03-29', 5, 5306.882);
